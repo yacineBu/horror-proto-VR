e39a272e2eaaf429bec59be11aafcb22988203d9
ServianB <benjamin.servian@edu.esiee.fr>
2024-06-06 14:30:19 +0200

    vr key system

--- Assets/key_system/KeyPickup.cs
+++ Assets/key_system/KeyPickup.cs
@@ -1,16 +1,41 @@
 using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
 
 public class KeyPickup : MonoBehaviour
 {
+    public XRController rightHandController; // Assign the right hand controller in the inspector
+    public InputHelpers.Button activationButton = InputHelpers.Button.Trigger; // Choose the button to use
+
+    private bool isHovering = false;
+
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
-            PlayerInventory playerInventory = other.GetComponent<PlayerInventory>();
-            if (playerInventory != null)
+            isHovering = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            isHovering = false;
+        }
+    }
+
+    private void Update()
+    {
+        if (isHovering && rightHandController)
+        {
+            if (InputHelpers.IsPressed(rightHandController.inputDevice, activationButton, out bool isPressed) && isPressed)
             {
-                playerInventory.PickupKey();
-                Destroy(gameObject); // Destroy the key object after picking it up
+                PlayerInventory playerInventory = rightHandController.GetComponentInParent<PlayerInventory>();
+                if (playerInventory != null)
+                {
+                    playerInventory.PickupKey();
+                    Destroy(gameObject); // Destroy the key object after picking it up
+                }
             }
         }
     }
