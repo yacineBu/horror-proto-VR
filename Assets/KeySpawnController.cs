0bf38893e52d4dfa095e088393034dbe7972fdac
victorinn <nathan.victorin@edu.esiee.fr>
2024-06-08 16:00:52 +0200

    spawn clé + ajout serrure porte de sortie + spawn player

--- Assets/KeySpawnController.cs
+++ Assets/KeySpawnController.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,7 +8,14 @@ public class KeySpawnController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        // Désactive toutes les clés au cas où
+        for (int i = 0; i < transform.childCount; i++)
+            transform.GetChild(i).gameObject.SetActive(false);
+        // Nombre aléatoire pour la clé
+        System.Random random = new System.Random();
+        int randomIndex = random.Next(0, transform.childCount);
+        // Active la clé choisi aléatoirement
+        transform.GetChild(randomIndex).gameObject.SetActive(true);
     }
 
     // Update is called once per frame
